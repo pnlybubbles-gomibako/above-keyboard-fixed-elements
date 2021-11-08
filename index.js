a876a2790a84de34b87775d239927d3ac4fbc93d
pnly <pnlybubbles@gmail.com>
2021-11-09 02:45:52 +0900

    .

---
+++ index.js
@@ -0,0 +1,37 @@
+const bottomBar = document.getElementById("bottombar");
+const viewport = window.visualViewport;
+const scrolling = document.getElementById("scrolling");
+
+// ref: https://developer.mozilla.org/en-US/docs/Web/API/VisualViewport
+function viewportHandler() {
+  const layoutViewport = document.getElementById("layoutViewport");
+  const bottomBarHeight = bottomBar.getBoundingClientRect().height;
+  scrolling.style.height = `${viewport.height - bottomBarHeight}px`;
+  scrolling.style.bottom = `${bottomBarHeight}px`;
+
+  const offsetTop =
+    viewport.height -
+    layoutViewport.getBoundingClientRect().height +
+    viewport.offsetTop;
+  console.log(viewport, layoutViewport.getBoundingClientRect())
+
+  const transformStyle = `translateY(${offsetTop}px)`;
+
+  bottomBar.style.transform = transformStyle;
+  scrolling.style.transform = transformStyle;
+}
+
+window.visualViewport.addEventListener("scroll", viewportHandler);
+window.visualViewport.addEventListener("resize", viewportHandler);
+bottomBar.addEventListener("touchmove", (e) => e.preventDefault(), {
+  passive: false,
+});
+scrolling.addEventListener("touchmove", (e) => e.stopPropagation());
+document.addEventListener("touchmove", (e) => e.preventDefault(), {
+  passive: false,
+});
+bottomBar.addEventListener("touchstart", viewportHandler)
+document.getElementById("text").addEventListener("focus", () => window.scrollTo(0, 0))
+
+document.body.style.minHeight = `${window.innerHeight}px`;
+viewportHandler();
